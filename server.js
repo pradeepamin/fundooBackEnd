a4db1d8e60509189f71dbee9b12c7d146b791df5
Pradeep B Amin <pradeepbamin5@gmail.com>
2019-11-29 17:59:13 +0530

    [Pradeep] Refactor. Fundoo server  modified and pushed into remote

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+/********************************************************************************************************************
+ * @Execution   : default nodemon : server.js
+ * @Purpose     : Backend for user Registration,Login,ForgotPassword,ResetPassword.
+ * @description : Using express frame work and socketio build a realtime cpplication
+ * @overview    : Fundoo BackEnd 
+ * @author      : PRADEEP B AMIN<pradeepbamin5@gmail.com>
+ * @version     : v12.10.0
+ * @since       : 29-NOV-2019
+ *
+ *******************************************************************************************************************/
+//importing express framework 
 const express=require('express');
 const bodyParser=require('body-parser');
 const routerUser=require('./router/userRouter');
@@ -11,7 +22,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(expressValidator());
 app.use('/', routerUser);
+console.log("process.env",process.env.PORT);
 
+//Initalizing the app port number,Telling frame work to start service
 app.listen(process.env.PORT, () => {
     console.log("Server is listing on port 4000")
     dbConnect.dbConnection();
