7feabb58752c4e9d5be2e0ff23627e78cca1f5a0
Pradeep B Amin <pradeepbamin5@gmail.com>
2019-11-29 17:58:47 +0530

    [Pradeep] Refactor. Fundoo userModel modified and pushed into remote

--- model/userModel.js
+++ model/userModel.js
@@ -1,10 +1,10 @@
-const mongoose=require('mongoose');
-const bcrypt=require('bcrypt');
-const emailExistence=require('email-existence');
- 
-var Schema=mongoose.Schema;
-
-const userData=new Schema({
+const mongoose = require("mongoose");
+const bcrypt = require('bcrypt');
+const emailExistance = require("email-existence");
+/*creating a schema of database*/
+var Schema = mongoose.Schema;
+const usersData = new Schema({
+    /** creating schema for registration */
     firstName: {
         type: String,
         required: true
@@ -25,39 +25,48 @@ const userData=new Schema({
     {
         timestamps: true
     }
-);
-var registredUser=mongoose.model("users",userData);
+)
 
-exports.Register=(req,callback)=>{
-    registredUser.findOne({"email":req.body.email},(err,data)=>{
-        if(data)callback("User Already Exists");
-        else{
-            emailExistence.check(req.bodu.email,(err,result)=>{
-                if(!result)callback("Provide vaild email")
-                else{
-                    bcrypt.hash(req.body.password,10,(err,encrypted)=>{
-                        var userDetails=new registredUser({
-                            "firstName":req.body.firstName,
-                            "lastName":req.body.lastName,
-                            "email":req.body.email,
-                            "password":encrypted
-                        })
-                        userDetails.save((err,data)=>{
-                            if(err){
-                                callback(err)
-                            }
-                            else{
-                                callback(null,data)
-                            }
-                        })
 
-                    })
-                }
-            })
-        }
-    } )
+
+var registerUsers = mongoose.model("users", usersData);
+
+
+/**
+ * @desc Gets the input from front end and stores data in deatabase
+ * @param req request contains all the requested data
+ * @param callback a callback function
+ * @return return a call back function err or data
+ */
+exports.Register = (req, callback) => {
+
+    try {
+        registerUsers.findOne({"email": req.body.email}, (err, data) => {
+            if (data) callback("user exits");
+            else {
+                emailExistance.check(req.body.email, (err, result) => {
+
+                    if (!result) callback("provide valid email")
+                    else {
+                        bcrypt.hash(req.body.password, 10, (err, encrypted) => {
+                            var userDetails = new registerUsers({
+                                "firstName": req.body.firstName,
+                                "lastName": req.body.lastName,
+                                "email": req.body.email,
+                                "password": encrypted
+                            })
+                            userDetails.save((err, data) => {
+                                if (err) {
+                                    callback(err);
+                                } else callback(null, data);
+                            })
+                        })
+                    }
+                })
+
+            } console.log("model end")
+        })
+    } catch (e) {
+        console.log(e);
+    }
 }
-
-
-
-
