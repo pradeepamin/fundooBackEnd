d7e40f3e3c20eda98824d7728979d6d98745a0d6
Pradeep B Amin <pradeepbamin5@gmail.com>
2019-11-30 15:40:14 +0530

    [Pradeep] Refactor fundoo added middleware for token verification and pushed into remote

--- middleware/token.js
+++ middleware/token.js
@@ -1,14 +1,30 @@
-const jwt=require('jsonwebtoken');
-// console.log("in token");
-exports.GenerateToken=(payload)=>{
-   {
-       //console.log("in token id",payload);
-       const token =  jwt.sign({payload}, process.env.KEY, { expiresIn:'1h' }) // expires in 1 hour
-       const obj = {        
-           success: true,
-           message: 'Token Generated Successfully!!',
-           token: token
-       }
-       return obj;
-   }
+console.log("in token");
+const jwt=require('jsonwebtoken')
+exports.generateToken=(payload)=>{
+    {
+        //console.log("in token id",payload);
+        console.log("token-payload-->",payload)
+        const token =  jwt.sign({payload}, process.env.KEY, { expiresIn:'1h' }) // expires in 1 hour
+        console.log("key------>",process.env.KEY)
+        const obj = {        
+            success: true,
+            message: 'Token Generated Successfully!!',
+            token: token
+        }
+        return obj;
+        
+    }
 }
+
+exports.verify=(req,res,next)=>{
+    console.log("verifies request");
+    var token=req.params.token;
+    jwt.verify(token,process.env.KEY,(err,result)=>{
+        if(err) res.status(422).send({message:"token is incorrect"});
+        else{
+            req.decoded=result;
+            next();
+        }
+    })
+}
+ 
