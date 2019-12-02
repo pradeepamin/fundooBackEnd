6cc5b48dbd8d0289de049ae778eb841f67fc3422
Pradeep B Amin <pradeepbamin5@gmail.com>
2019-12-02 17:57:54 +0530

    [Pradeep] Refactor fundoo updated note service and pushed into remote

--- services/noteServices.js
+++ services/noteServices.js
@@ -4,20 +4,23 @@ const noteModel = require('../model/noteModel');
 exports.addUser = (req) => {
     console.log(req.body);
     return new Promise((resolve, reject) => {
+        console.log("req.decoded",req.decoded);
+        
         let noteDetails = new noteModel.notes({
+            "_userId":req.decoded.payload.id,
             "title": req.body.title,
-            "discription": req.body.discription
+            "description": req.body.description
         })
         noteDetails.save((err, data) => {
             if (data) {
                 resolve(data);
+
             } else {
 
                 reject(err);
             }
         })
     })
-
 }
 
 
