b9cddc2254f8f85c1577025859fe9b91468577a7
Pradeep B Amin <pradeepbamin5@gmail.com>
2019-12-02 15:50:40 +0530

    [Pradeep] Add. Created api for addNote and pushed into remote

---
+++ controller/noteController.js
@@ -0,0 +1,36 @@
+const noteService = require('../services/noteServices')
+
+exports.addNote = (req, res) => {
+
+    req.checkBody('title', 'discription is invalid').notEmpty();
+    req.checkBody('discription', 'discription is invalid').notEmpty();
+
+    var error = req.validationErrors();
+    var response = {};
+    if (error) {
+        response.error = error;
+        response.sucess = false;
+        res.status(422).send(response);
+        console.log("error-register", error);
+        console.log("Response", response)
+    } else {
+        console.log("Req body---->",req.body)
+        noteService.addUser(req)
+            .then((data) => {
+                
+                console.log("In conttoller");
+                response.sucess=true;
+                response.data=data
+                // res.status(200).send(response)
+                console.log(response);
+                
+            })
+            .catch((err) => {
+                response.sucess=false;
+                response.err=err
+                res.status(404).send(response)
+            })
+
+    
+}
+}
