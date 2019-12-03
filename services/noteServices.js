6c301f1f0e2e7626f5ae2e36f9a375734bdc4626
Pradeep B Amin <pradeepbamin5@gmail.com>
2019-12-03 19:16:34 +0530

    [Pradeep] Add . Added service for note api and pushed into remote

--- services/noteServices.js
+++ services/noteServices.js
@@ -1,13 +1,13 @@
 
 const noteModel = require('../model/noteModel');
 
+
 exports.addUser = (req) => {
     console.log(req.body);
     return new Promise((resolve, reject) => {
-        console.log("req.decoded",req.decoded);
-        
+        console.log("req.decoded", req.decoded);
         let noteDetails = new noteModel.notes({
-            "_userId":req.decoded.payload.id,
+            "_userId": req.decoded.payload.id,                                //req.decoded.payload.id,
             "title": req.body.title,
             "description": req.body.description
         })
@@ -21,4 +21,43 @@ exports.addUser = (req) => {
     })
 }
 
+exports.getAllNote = (req) => {
+    console.log("req in get all notes------------->", req);
+    return new Promise((resolve, reject) => {
+        noteModel.notes.find({ _userId: req.decoded.payload.id, isDeleted: false }, (err, result) => {
+            if (err) {
+                reject(err)
+            } else {
+                resolve(result)
+            }
+        })
 
+    })
+
+}
+
+exports.deleteNote = (req) => {
+    console.log("REQQ------>", req);
+    return new Promise((resolve, reject) => {
+        noteModel.notes.findByIdAndUpdate({ _id: req.body.userId }, { isDeleted: true }, (err, result) => {
+            if (err) {
+                reject(err)
+            } else {
+                resolve(result)
+
+            }
+        })
+
+    })
+}
+
+
+exports.updateNote = (req) => {
+    return new Promise((resolve, reject) => {
+        noteModel.notes.findByIdAndUpdate({
+            _id: req.body._id}, {title: req.body.title,description: req.body.description}, (err, data) => {
+                if (err) reject(err)
+                else resolve(data)
+            })
+    })
+}
