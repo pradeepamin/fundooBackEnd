5e6c65d0044835a8ca1a651aef9f94c577cbc365
Pradeep B Amin <pradeepbamin5@gmail.com>
2019-12-18 19:28:27 +0530

    [Pradeep] Add. Added SNS services  and pushed into remote

---
+++ AWS_Mail.js
@@ -0,0 +1,39 @@
+
+require('dotenv').config();
+const AWS = require('aws-sdk');
+
+exports.notification = (details) => {
+    return new Promise((resolve, reject) => {
+
+    console.log("notification", details.name);
+
+    AWS.config.getCredentials(function (err) {
+        if (err) console.log(err.stack);
+        // credentials not loaded
+        else {
+        console.log("Access key:", AWS.config.credentials.accessKeyId);
+        console.log("Secret access key:", AWS.config.credentials.secretAccessKey);
+        }
+        });
+    
+    AWS.config.update({ region: process.env.AWS_REGION });
+
+    let params = {
+        Message: `You have a reminder : ${details.name} and title : ${details.index}`, /* required */
+        TopicArn: process.env.AWS_TOPIC_ARN
+    };
+
+    let publishTextPromise = new AWS.SNS().publish(params).promise();
+
+    publishTextPromise.then(
+        function (data) {
+            resolve(data)
+            console.log(`message ${params.Message} send sent to the topic ${params.TopicArn}`);
+            console.log("MessageID is ", data.MessageId);
+        }).catch(
+            function (err) {
+                reject(err)
+                console.error(err, err.stack);
+            });
+        })
+}
