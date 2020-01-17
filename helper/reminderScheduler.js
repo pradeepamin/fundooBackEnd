3677ab1ea7ef75ed4b6e8d844b7ba1ad95ab570c
Pradeep B Amin <pradeepbamin5@gmail.com>
2020-01-17 19:25:34 +0530

    [Pradeep]Refactor. made some changes and pushed into remote

--- helper/reminderScheduler.js
+++ helper/reminderScheduler.js
@@ -1,24 +1,29 @@
 let schedule = require('node-schedule')
 let sns = require('../AWS_Mail')
 let services = require('../services/noteServices')
+let noteCon=require('../controller/noteController')
 
-exports.scheduleReminder = (req) => {
+
+exports.scheduleReminder =(req) => {
     let details;
-    console.log("reqqq---<", req.reminder);
+    console.log("reqqq---<", req.email);
+    console.log("reqqq---<", req.resolve.reminder);
+
+    let email=req.email;
+
 
     return new Promise((resolve, reject) => {
-         let date2 = new Date(req.reminder);
-        // console.log("date--->",date);
+         let date2 = new Date(req.resolve.reminder);
+        //  console.log("reminder date--->",date);
 
-        // var date = new Date(2020, 0, 06, 9, 50, 0);
+        var prp = new Date(2020, 0, 14, 12, 15, 0);
+        console.log("Prprrp-0",prp);
         
-    
-        // console.log("date in iso conversion--->",isoPost);        
-        // var date1 = new Date(req.reminder);
-
         console.log("date--->",date2);
 
-        var j = schedule.scheduleJob(date2, function () {
+        var j = schedule.scheduleJob(date2,  function () {
+            
+
             console.log('The world is going to end today.');
                 console.log("true");
                 const arr={
@@ -26,7 +31,13 @@ exports.scheduleReminder = (req) => {
                     "index":"pradeep",
                     "name":"amin"
                   }
-                  sns.notification(arr);
+
+                
+                  noteCon.resData("You have one reminder")
+                  sns.notification(arr,email);
+                  resolve('triggered')
+
+                
             
         })
     });
