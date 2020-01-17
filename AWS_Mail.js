059afe691733d7cf712202e13a7dc7f2f677ec55
Pradeep B Amin <pradeepbamin5@gmail.com>
2020-01-17 19:25:23 +0530

    [Pradeep]Refactor. made some changes and pushed into remote

--- AWS_Mail.js
+++ AWS_Mail.js
@@ -2,7 +2,9 @@
 require('dotenv').config();
 const AWS = require('aws-sdk');
 
-exports.notification = (details) => {
+exports.notification = (details,email) => {
+    console.log("Details",details,"email---",email);
+    
     return new Promise((resolve, reject) => {
 
     console.log("notification", details.name);
@@ -13,14 +15,16 @@ exports.notification = (details) => {
         else {
         console.log("Access key:", AWS.config.credentials.accessKeyId);
         console.log("Secret access key:", AWS.config.credentials.secretAccessKey);
-        }
+        }                                                                                                                           
         });
-    
+
     AWS.config.update({ region: process.env.AWS_REGION });
 
     let params = {
         Message: `You have a reminder : ${details.name} and title : ${details.index}`, /* required */
-        TopicArn: process.env.AWS_TOPIC_ARN
+        TopicArn: process.env.AWS_TOPIC_ARN,
+        // Protocol: 'EMAIL',
+        // Endpoint: email
     };
 
     let publishTextPromise = new AWS.SNS().publish(params).promise();
